9111e7e91b072edf22aec861187b168a90c40581
andesob <andesob@stud.ntnu.no>
2019-02-27 15:09:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,50 @@
+import java.util.ArrayList;
 
 /**
- * The Main-class of the application. This class only holds the main()-method
- * to start the application.
- * 
- * @author asty
- * @version 1.0
+ * The type Main.
  */
-public class Main 
-{
+public class main {
     /**
-     * The main entry for the application.
-     * @param args arguments provided during startup of the application
+     * The entry point of application.
+     *
+     * @param args the input arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        String paragraph = "------------------------------------";
+
+
         ApplicationUI appUI = new ApplicationUI();
         appUI.start();
-    }
-}
+
+       /* Registry registry1 = new Registry();
+        registry1.fillRegistryWithDummies();
+        System.out.println("Number of books: " + registry1.getNumberOfBooks());
+        registry1.removeBookByTitle("bruh");
+        System.out.println("Number of books: " + registry1.getNumberOfBooks());
+
+        System.out.println(paragraph);
+        System.out.println("Books by Eskil: ");
+        ArrayList<Book> author = registry1.findBookByAuthor("eskil");
+        for (Book b : author) {
+            System.out.println(b.listAllInfo());
+            System.out.println();
+        }
+
+        System.out.println(paragraph);
+        registry1.listAllBooks();
+        System.out.println(paragraph);
+        registry1.searchBookByIndex(0);
+        registry1.removeBookByIndex(8);
+        registry1.listByTitle("trondheim");
+        Book tjommi = registry1.findBookByTitle("Tjommi");
+        tjommi.convertToSeries("Acer");
+       // registry1.convertToSeries2(tjommi,"Acer");
+
+        System.out.println(paragraph);
+        ArrayList<Book> publisher = registry1.findBookByPublisher("NTNU");
+        for (Book b : publisher) {
+            System.out.println(b.listAllInfo());
+            System.out.println();
+        }
+    }*/
+} }
