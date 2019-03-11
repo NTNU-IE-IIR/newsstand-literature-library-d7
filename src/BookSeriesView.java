ff11a2b56ee74fad818dfc55b8b322595c5cba11
andesob <andesob@stud.ntnu.no>
2019-03-11 23:04:51 +0100

    Updated 8 files

--- src/BookSeriesView.java
+++ src/BookSeriesView.java
@@ -1,12 +1,11 @@
-public class BookSeriesView {
-    private BookSeries bookSeries;
+import java.util.Scanner;
 
-    public BookSeriesView(BookSeries book){
-        this.bookSeries = book;
-        viewBook();
+public class BookSeriesView {
+
+    public BookSeriesView(){
     }
 
-    private void viewBook(){
+    public static void viewBookSeries(BookSeries bookSeries){
         String title = bookSeries.getTitle();
         String seriesTitle = bookSeries.getSeriesTitle();
         String author = bookSeries.getAuthor();
@@ -17,7 +16,7 @@ public class BookSeriesView {
         printInfo(title, author, date, publisher, edition, seriesTitle);
     }
 
-    private void printInfo(String title, String author, String date, String publisher, String edition, String seriesTitle){
+    private static void printInfo(String title, String author, String date, String publisher, String edition, String seriesTitle){
         System.out.println("-------------------------------------");
         System.out.println("Title: " + title
                         + "\nSeries Title: " + seriesTitle
@@ -26,5 +25,59 @@ public class BookSeriesView {
                         + "\nPublisher: " + publisher
                         + "\nEdition: " + edition);
         System.out.println("-------------------------------------");
+    }
+
+    public static Literature addBookSeries(){
+        System.out.println("\nWhat is the name of the author?");
+        Scanner reader = new Scanner(System.in);
+        String author = reader.nextLine();
+
+        System.out.println("\nWhat is the title of the bookseries?");
+        String title = reader.nextLine();
+
+        System.out.println("\nWho published the bookseries?");
+        String publisher = reader.nextLine();
+
+        System.out.println("\nWhich edition of the bookseries is it?");
+        String edition = reader.nextLine();
+
+        System.out.println("\nWhat date was the bookseries published?");
+        String date = reader.nextLine();
+
+        System.out.println("\nWhat is the series title?");
+        String seriesTitle = reader.nextLine();
+
+        int errors = 0;
+
+        if (author.length() == 0) {
+            System.out.println("\nThe bookseries has to have an author");
+            errors += 1;
+        }
+        if (title.length() == 0) {
+            System.out.println("\nThe bookseries has to have a title");
+            errors += 1;
+        }
+        if (publisher.length() == 0) {
+            System.out.println("\nThe bookseries has to have a publisher");
+            errors += 1;
+        }
+        if (edition.length() == 0) {
+            System.out.println("\nYou didn't insert which edition the bookseries was");
+            errors += 1;
+        }
+        if (date.length() == 0) {
+            System.out.println("\nYou didn't insert a release date for the book");
+            errors += 1;
+        }
+        if (seriesTitle.length()==0){
+            System.out.println("\n The bookseries has to have a seriestitle");
+            errors +=1;
+        }
+
+        if (errors > 0) {
+            System.out.println("\nYou had " + errors + " errors when adding the bookseries, please try again");
+            return null;
+        }
+        return new BookSeries(author, title, publisher, edition, date, seriesTitle);
     }
 }
--- src/NewspaperView.java
+++ src/NewspaperView.java
@@ -1,12 +1,11 @@
-public class NewspaperView {
-    private Newspaper newspaper;
+import java.util.Scanner;
 
-    public NewspaperView(Newspaper newspaper){
-        this.newspaper = newspaper;
-        viewNewspaper();
+public class NewspaperView {
+
+    public NewspaperView() {
     }
 
-    private void viewNewspaper(){
+    public static void viewNewspaper(Newspaper newspaper) {
         String title = newspaper.getTitle();
         String publisher = newspaper.getPublisher();
         String genre = newspaper.getGenre();
@@ -15,12 +14,57 @@ public class NewspaperView {
         printInfo(title, publisher, genre, releases);
     }
 
-    private void printInfo(String title, String publisher, String genre, int releases){
+    private static void printInfo(String title, String publisher, String genre, int releases) {
         System.out.println("-------------------------------------");
         System.out.println("Title: " + title
-                        + "\nPublisher: " + publisher
-                        + "\nGenre: " + genre
-                        + "\nReleases: " + releases);
+                + "\nPublisher: " + publisher
+                + "\nGenre: " + genre
+                + "\nReleases: " + releases);
         System.out.println("-------------------------------------");
+    }
+
+    public static Literature addNewspaper(){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("\nWhat is the title of the newspaper you want to add?");
+        String title = reader.nextLine();
+
+        System.out.println("\nWho published the newspaper?");
+        String publisher = reader.nextLine();
+
+        System.out.println("\nWhat is the genre of the newspaper?");
+        String genre = reader.nextLine();
+
+        int releasesPerYear = 0;
+        System.out.println("\nHow many releases per year has the newspaper?");
+        if (reader.hasNextInt()) {
+            releasesPerYear = reader.nextInt();
+        }
+
+        int errors = 0;
+
+        if (title.length() == 0) {
+            System.out.println("\nThe newspaper has to have a title");
+            errors += 1;
+        }
+        if (publisher.length() == 0) {
+            System.out.println("\nThe newspaper has to have a publisher");
+            errors += 1;
+        }
+        if (genre.length() == 0) {
+            System.out.println("\nThat newspaper has to have a genre");
+            errors += 1;
+        }
+
+        if (releasesPerYear == 0) {
+            System.out.println("\nThe number for releases per year is not valid, please try again");
+            errors += 1;
+        }
+
+        if (errors > 0) {
+            System.out.println("\nYou had " + errors + " errors when adding the newspaper, please try again");
+            return null;
+        }
+         return new Newspaper(title, publisher, genre, releasesPerYear);
     }
 }
