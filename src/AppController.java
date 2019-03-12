be684cb4607dad346e2f88701b1d331113097563
andesob <andesob@stud.ntnu.no>
2019-03-13 00:06:49 +0100

    Updated
    bare gui løk

--- src/AppController.java
+++ src/AppController.java
@@ -2,29 +2,42 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class AppController {
     private GUIApp gui;
     private Stage stage;
+    private Registry literatureRegistry;
 
     public AppController(Stage stage, GUIApp gui) {
         this.gui = gui;
         this.stage = stage;
+        literatureRegistry = new Registry();
 
         CancelProcess cancelProcess = new CancelProcess();
         gui.getCancelButton().setOnAction(cancelProcess);
 
-        NewspaperButtonEvent NewspaperButtonEvent = new NewspaperButtonEvent();
-        gui.getBookButton().setOnAction(NewspaperButtonEvent);
+        NewspaperButtonEvent newspaperButtonEvent = new NewspaperButtonEvent();
+        gui.getNewspaperButton().setOnAction(newspaperButtonEvent);
+
+        ComicButtonEvent comicButtonEvent = new ComicButtonEvent();
+        gui.getComicButton().setOnAction(comicButtonEvent);
+
+        BookButtonEvent bookButtonEvent = new BookButtonEvent();
+        gui.getBookButton().setOnAction(bookButtonEvent);
+
+        AddLiteratureEvent addLiteratureEvent = new AddLiteratureEvent();
+        gui.getAddLiteratureButton().setOnAction(addLiteratureEvent);
+
+        ListLiteratureEvent listLiteratureEvent = new ListLiteratureEvent();
+        gui.getListLiteratureButton().setOnAction(listLiteratureEvent);
     }
 
 
@@ -35,47 +48,201 @@ public class AppController {
         }
     }
 
-    class NewspaperButtonEvent implements EventHandler<ActionEvent> {
+
+    class AddLiteratureEvent implements EventHandler<ActionEvent> {
         @Override
-        public void handle(ActionEvent e) {
-            gui.getNewspaperButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
+        public void handle(ActionEvent event) {
+            gui.getGridPane().getChildren().clear();
+
+        }
+    }
+
+    class ListLiteratureEvent implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            gui.getGridPane().getChildren().clear();
+            ArrayList<Literature> literatureList = literatureRegistry.getLiteratureList();
+            gui.getQuestion().setText("Here is all the literature in the list:");
+            int i = 0;
+            for (Literature l : literatureList) {
+                String title = l.getTitle();
+                gui.getGridPane().add(new Text(title), 0, i);
+                i++;
+            }
+        }
+    }
+
+
+    abstract class LiteratureButtonEvent implements EventHandler<ActionEvent> {
+        protected Button submitButton;
+        protected Button returnButton;
+        protected HBox buttonBox;
+        protected Label label;
+        protected TextField field1;
+        protected TextField field2;
+        protected TextField field3;
+        protected TextField field4;
+
+        @Override
+        public abstract void handle(ActionEvent event);
+
+        public void setup(Text text1, Text text2, Text text3, Text text4) {
+            field1 = new TextField();
+            field2 = new TextField();
+            field3 = new TextField();
+            field4 = new TextField();
+
+            gui.getGridPane().add(text1, 0, 0);
+            gui.getGridPane().add(text2, 0, 2);
+            gui.getGridPane().add(text3, 0, 4);
+            gui.getGridPane().add(text4, 0, 6);
+            gui.getGridPane().add(field1, 0, 1);
+            gui.getGridPane().add(field2, 0, 3);
+            gui.getGridPane().add(field3, 0, 5);
+            gui.getGridPane().add(field4, 0, 7);
+
+            submitButton = new Button("Submit");
+            returnButton = new Button("Return");
+            buttonBox = new HBox(submitButton, returnButton);
+            label = new Label();
+
+            returnButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    gui.getGridPane().getChildren().clear();
+                    gui.getGridPane().add(gui.getListLiteratureButton(), 0, 1);
+                    gui.getGridPane().add(gui.getAddLiteratureButton(), 1, 1);
+                    gui.getGridPane().add(gui.getFindByTitleButton(), 2, 1);
+                    gui.getGridPane().add(gui.getFindByAuthorButton(), 3, 1);
+                    gui.getRoot().setTop(gui.getQuestion());
+                }
+            });
+        }
+    }
+
+    class BookButtonEvent extends LiteratureButtonEvent implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            gui.getGridPane().getChildren().clear();
+            gui.getRoot().setTop(new Text(""));
+            Text title = new Text("What is the title of the book?");
+            Text publisher = new Text("Who is the publisher?");
+            Text author = new Text("Who is the author?");
+            Text edition = new Text("What edition is it?");
+            Text publishDate = new Text("What date was the book released?");
+
+            TextField dateField = new TextField();
+
+            setup(title, publisher, author, edition);
+
+            gui.getGridPane().add(publishDate, 0, 8);
+            gui.getGridPane().add(dateField, 0, 9);
+            gui.getGridPane().add(buttonBox, 0, 10);
+
+
+            submitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (!field1.getText().isEmpty() && !field2.getText().isEmpty() && !field3.getText().isEmpty()
+                            && !field4.getText().isEmpty() && !dateField.getText().isEmpty()) {
+                        label.setText("You added a book titled " + field1.getText()
+                                + "\nthat is published by " + field2.getText()
+                                + "\nand authored by " + field3.getText());
+                        literatureRegistry.addLiterature(new Book(field3.getText(), field1.getText(), field2.getText(), field4.getText(), dateField.getText()));
+                        field1.clear();
+                        field2.clear();
+                        field3.clear();
+                        field4.clear();
+                        dateField.clear();
+                    } else {
+                        label.setText("You have to enter text in all the fields");
+                    }
+                    gui.getGridPane().add(label, 0, 11);
+                }
+            });
+        }
+    }
+
+    class ComicButtonEvent extends LiteratureButtonEvent implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            gui.getFindByTitleButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
                     gui.getGridPane().getChildren().clear();
+                    gui.getRoot().setTop(new Text(""));
+                    Text title = new Text("What is the title of the Comic Book?");
+                    Text publisher = new Text("Who is the publisher?");
+                    Text serialNumber = new Text("What is the serial number?");
+                    Text publishDate = new Text("What date was it released?");
+
+                    setup(title, publisher, serialNumber, publishDate);
+
+                    gui.getGridPane().add(buttonBox, 0, 8);
+
+                    submitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            if (!field1.getText().isEmpty() && !field2.getText().isEmpty()
+                                    && !field3.getText().isEmpty() && !field4.getText().isEmpty()) {
+                                try {
+                                    int i = Integer.parseInt(field3.getText());
+                                    label.setText("You added a comic book titled " + field1.getText()
+                                            + " that is published by " + field2.getText());
+
+                                } catch (NumberFormatException nfe) {
+                                    label.setText("You have to enter a valid number for the serial number");
+                                }
+                            } else {
+                                label.setText("You have to enter text in all the fields");
+                            }
+                        }
+                    });
+                    gui.getGridPane().add(label, 0, 9);
+                }
+            });
+        }
+    }
+
+    class NewspaperButtonEvent extends LiteratureButtonEvent implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent e) {
+            gui.getAddLiteratureButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    gui.getGridPane().getChildren().clear();
+                    gui.getRoot().setTop(new Text(""));
                     Text title = new Text("What is the title of the newspaper?");
                     Text publisher = new Text("Who is the publisher?");
                     Text genre = new Text("What kind of newspaper is it?");
                     Text releases = new Text("How many times a year does it release?");
 
-                    TextField titleField = new TextField();
-                    TextField publisherField = new TextField();
-                    TextField genreField = new TextField();
-                    TextField releasesField = new TextField();
-                    gui.getGridPane().add(title, 0, 0);
-                    gui.getGridPane().add(titleField,0, 1);
-                    gui.getGridPane().add(publisher,0, 2);
-                    gui.getGridPane().add(publisherField,0, 3);
-                    gui.getGridPane().add(genre, 0, 4);
-                    gui.getGridPane().add(genreField,0, 5);
-                    gui.getGridPane().add(releases, 0, 6);
-                    gui.getGridPane().add(releasesField,0, 7);
+                    setup(title, publisher, genre, releases);
 
-                    Button submitButton = new Button("Submit");
-                    gui.getGridPane().add(submitButton, 0, 8);
+                    gui.getGridPane().add(buttonBox, 0, 8);
 
                     submitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent event) {
-                            Label label = new Label();
-                            if((title.getText() != null) && !title.getText().isEmpty()){
-                                label.setText("You added a newspaper titled " + titleField.getText()
-                                        + " that is published by " + publisherField.getText());
-                                gui.getGridPane().add(label, 0, 9);
+                            if (!field1.getText().isEmpty() && !field2.getText().isEmpty()
+                                    && !field3.getText().isEmpty() && !field4.getText().isEmpty()) {
+                                try {
+                                    int i = Integer.parseInt(field4.getText());
+                                    label.setText("You added a newspaper titled " + field1.getText()
+                                            + " that is published by " + field2.getText());
+
+                                } catch (NumberFormatException nfe) {
+                                    label.setText("You have to enter a valid number for releases per year");
+                                }
+                            } else {
+                                label.setText("You have to enter text in all the fields");
                             }
                         }
                     });
+                    gui.getGridPane().add(label, 0, 9);
                 }
             });
+
         }
     }
 }
--- src/GUIApp.java
+++ src/GUIApp.java
@@ -1,4 +1,3 @@
-import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -14,9 +13,19 @@ public class GUIApp {
     private Button cancelButton;
     private GridPane gridPane;
 
+    private Button listLiteratureButton;
+    private Button addLiteratureButton;
+    private Button findByTitleButton;
+    private Button findByAuthorButton;
+    private Button removeByTitleButton;
+    private Button convertBookButton;
+
     private Button bookButton;
     private Button newspaperButton;
     private Button comicButton;
+    private Button bookSeriesButton;
+
+    private Text question;
 
     public Button getBookButton() {
         return bookButton;
@@ -28,6 +37,38 @@ public class GUIApp {
 
     public Button getComicButton() {
         return comicButton;
+    }
+
+    public Button getBookSeriesButton() {
+        return bookSeriesButton;
+    }
+
+    public Button getRemoveByTitleButton() {
+        return removeByTitleButton;
+    }
+
+    public Button getConvertBookButton() {
+        return convertBookButton;
+    }
+
+    public Button getFindByAuthorButton() {
+        return findByAuthorButton;
+    }
+
+    public Text getQuestion() {
+        return question;
+    }
+
+    public Button getListLiteratureButton() {
+        return listLiteratureButton;
+    }
+
+    public Button getAddLiteratureButton() {
+        return addLiteratureButton;
+    }
+
+    public Button getFindByTitleButton() {
+        return findByTitleButton;
     }
 
     public Button getCancelButton() {
@@ -43,7 +84,6 @@ public class GUIApp {
     }
 
 
-
     public GUIApp(Stage stage) {
         this.stage = stage;
         root = new BorderPane();
@@ -53,12 +93,19 @@ public class GUIApp {
 
     private void setupGUI() {
 
-        Text question = new Text("What type of literature do you want to add?");
+        question = new Text("What do you want to do?");
 
         cancelButton = new Button("Cancel");
-        bookButton = new Button("Book");
+        listLiteratureButton = new Button("List all literature");
+        addLiteratureButton = new Button("Add more literature");
+        findByTitleButton = new Button("Find literature by title");
+        findByAuthorButton = new Button("Find book by author");
+        removeByTitleButton = new Button("Remove literature by title");
+        convertBookButton = new Button("Convert book to bookseries");
         newspaperButton = new Button("Newspaper");
         comicButton = new Button("Comic Book");
+        bookButton = new Button("Book");
+        bookSeriesButton = new Button("Bookseries");
 
 
         gridPane = new GridPane();
@@ -67,11 +114,16 @@ public class GUIApp {
         gridPane.setVgap(5);
         gridPane.setHgap(5);
 
-        gridPane.add(bookButton, 0, 1);
-        gridPane.add(newspaperButton, 1, 1);
-        gridPane.add(comicButton, 2, 1);
+        gridPane.add(listLiteratureButton, 0, 1);
+        gridPane.add(addLiteratureButton, 1, 1);
+        gridPane.add(findByTitleButton, 2, 1);
+        gridPane.add(findByAuthorButton, 0, 2);
+        gridPane.add(removeByTitleButton, 1, 2);
+        gridPane.add(convertBookButton, 2, 2);
+        gridPane.add(bookButton, 3, 0);
         root.setCenter(gridPane);
         root.setTop(question);
+        root.setAlignment(question, Pos.TOP_CENTER);
         root.setBottom(new HBox(cancelButton));
     }
 }
